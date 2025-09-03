845699848f18af5d9c4a63bbaae023e182f3b307
Lukas <luk.fwkb@yahoo.com>
2025-09-03 17:50:45 +0200

    Create mytoken.py

---
+++ mytoken.py
@@ -0,0 +1,3 @@
+# Für die Sicherheit und Speicherung des Discord-Bot Token ist jeder selber verantwortlich! Als Beispiel ist hier die Speicherung im Klartext, wie sie in allen unserer Codes der einfachkeit halber genutzt wird.
+
+TOKEN1 = "t1ok1332421e23134n324234234-32423423Mwgehfwdsalk2
